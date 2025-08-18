42a4bbd50bf1ec274eef500d38aefa3349abb3a4
N1nj4s <leonardozola04@gmail.com>
2025-08-18 20:37:18 -0300

    em andamento

--- GranStore/Controllers/HomeController.cs
+++ GranStore/Controllers/HomeController.cs
@@ -15,27 +15,24 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        Categoria categoria = new();
-        categoria.Id = 1;
-        categoria.Nome = "Eletronicos";
+        List<Categoria> categorias = [
+            new() {Id = 1, Nome = "Playstation2"},
+            new() {Id = 2, Nome = "Playstation3"},
+            new() {Id = 3, Nome = "Playstation4"},
+            new() {Id = 4, Nome = "Playstation5"},
+            new() {Id = 5, Nome = "Xbox 360"},
+            new() {Id = 6, Nome = "Xbox One"},
+            new() {Id = 7, Nome = "Xbox Series S"},
+            new() {Id = 8, Nome = "Xbox Series X"},
 
-        Categoria categoria2 = new()
-        {
-            Id = 2,
-            Nome = "Roupas"
-        };
-
-        List<Produto> produtos = [
-            new() {
-                Id = 1,
-                Nome = "Notebook G15",
-                Descricao = "Lorem Ipsum",
-                QtdeEstoque = 5,
-                ValorCusto = 2000m,
-                ValorVenda = 5500m,
-                Categoria = categoria
-            }
         ];
+
+        List<Produto> produtos = new List<Produto>
+        {
+            new Produto {  Id = 1, Categoria = categorias[1], Nome = "gta san andreas ps2", Descricao = "Grand Theft Auto: San Andreas (GTA: San Andreas) é um jogo de ação e aventura desenvolvido pela Rockstar North e publicado pela Rockstar Games. Foi lançado originalmente em 2004 para PlayStation 2", ValorCusto = 199,ValorVenda = 188, QtdeEstoque = 20, Foto = "/img/GTASA.png"},
+            new Produto {  Id = 2, Categoria = categorias[1], Nome = "Resident Evil 4 ps2", Descricao = "Resident Evil 4 é um jogo de ação e terror de sobrevivência desenvolvido e publicado pela Capcom. Lançado originalmente em 2005 para PlayStation 2, ele revolucionou a franquia Resident Evil ao introduzir uma jogabilidade mais voltada para a ação, mantendo ainda o clima sombrio e tenso característico da série.", ValorCusto = 249,ValorVenda = 229, QtdeEstoque = 10, Foto = "/img/RE4.png"},
+            new Produto {  Id = 3, Categoria = categorias[1], Nome = "Scooby Doo! First Frights ps2", Descricao = "Scooby-Doo! First Frights é um jogo de ação e aventura baseado na famosa franquia Scooby-Doo, desenvolvido pela Torus Games e publicado pela Warner Bros. Interactive Entertainment. Foi lançado em 2009 para PlayStation 2", ValorCusto = 150,ValorVenda = 80, QtdeEstoque = 10, Foto = "/img/SCFF.png"},
+        };
 
         return View();
     }
